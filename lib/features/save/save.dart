fbea88d3cac96520e85715c9b58e35f4d850a477
raghad <iamreee8@gmail.com>
2024-12-16 21:12:34 +0300

    init

--- lib/features/save/save.dart
+++ lib/features/save/save.dart
@@ -7,34 +7,52 @@ import 'package:base_project/features/home/view/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
 
-class SaveItemPageScreen extends StatelessWidget {
+class SaveItemPageScreen extends StatefulWidget {
   final Product product;
 
   SaveItemPageScreen({required this.product});
 
   @override
+  State<SaveItemPageScreen> createState() => _SaveItemPageScreenState();
+}
+
+class _SaveItemPageScreenState extends State<SaveItemPageScreen> {
+  double _scrollOffset = 0.0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffF5F5F5),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 300,
-            leading: IconButton(
-              icon: Icon(Icons.close, color: Colors.black),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollInfo) {
+          setState(() {
+            _scrollOffset = scrollInfo.metrics.pixels;
+          });
+          return true;
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 300,
+              pinned: true, // Keeps the AppBar visible when scrolling
+              leading: IconButton(
+                icon: Icon(Icons.close, color: Colors.black),
+                onPressed: () => Navigator.pop(context),
+              ),
+              flexibleSpace: FlexibleSpaceBar(
+                background: SliverAppBarWidget(product: widget.product),
+              ),
             ),
-            flexibleSpace: FlexibleSpaceBar(
-              background: SliverAppBarWidget(product: product),
+            SliverToBoxAdapter(
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 200),
+                color: _scrollOffset > 150 ? Colors.white : Colors.blueGrey[50],
+                child: SliverBodyWidget(),
+              ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: SliverBodyWidget(),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavWidget(
         text: "+ Create drawer",
@@ -73,7 +91,9 @@ class SliverBodyWidget extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: CustomSearchWidget(text:  "Search for a drawer",),
+            child: CustomSearchWidget(
+              text: "Search for a drawer",
+            ),
           ),
           Container(
             color: Colors.white,
@@ -140,7 +160,8 @@ class SliverBodyWidget extends StatelessWidget {
 class CustomSearchWidget extends StatelessWidget {
   final String text;
   const CustomSearchWidget({
-    super.key, required this.text,
+    super.key,
+    required this.text,
   });
 
   @override
